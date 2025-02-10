a6ae01ed705b8b70e3c084e1e12f53d5263c8632
Fang ZhiJie <funjee126@qq.com>
2025-02-10 23:52:47 +0800

    修改

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://blog.ltlyl.fun/react-clone/", // 修改为完整的URL
+  base: "./",
   plugins: [
     react({
       jsxRuntime: "classic",
